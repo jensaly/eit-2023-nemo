1b2172d82e7ce66fc657f4fbf8aad3db76163cd0
spraymon <jens.lyngaas@gmail.com>
2023-03-01 09:01:35 +0100

    New cmakelist solution, updated non-graphical main.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@
 
 int main() {
     srand(time(NULL));
-    Yard y = Yard(DoNothing(), DoNothing(), 5, 60, 4);
+    Yard y = Yard(BasicRules(), BasicRules(), 5, 60, 4);
     y.queues[0].SetReservedFlag(VehicleFlags::Ambulance);
     y.queues[0].SetReservedFlag(VehicleFlags::HC);
     y.queues[2].SetPriorityFlag(VehicleFlags::Heavy);
     Ferry ferry{5, 17.8, 1.9, 5 * 1.9, 18.0};
+    y.SimulteQueueArrival(std::gamma_distribution(1.4, 1.5), 60);
+    y.Embark(ferry);
 
     CURL *curl;
     CURLcode res;
