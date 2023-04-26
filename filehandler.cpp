8b8b70dfa172d7ded7d0e382fd28174b7854055c
jensaly <jens.lyngaas@gmail.com>
2023-04-26 06:02:51 +0200

    Made the yard output be sorted such that the order reflects the order in which the cars arrived.

--- filehandler.cpp
+++ filehandler.cpp
@@ -22,14 +22,20 @@ void FileHandler::Write(Ferry& f, Yard& y) {
             ofile << v.type << " " << v.plate_nr << " " << v.arrival_time << " " << std::to_string(i) << " " << std::to_string(j) << " " << v.x << " " << v.y << " " << v.length << " " << v.width << " " << v.mass << "\n";
         }
     }
+    std::vector<std::tuple<Vehicle*, size_t, size_t>> veh_ptr;
+    for (size_t i = 0; i < y.queues.size(); i++) {
+        auto& q = y.queues[i];
+        for (size_t j = 0; j < q.vehicles.size(); j++) {
+            auto& v = q.vehicles[j];
+            veh_ptr.push_back({&v, i, j});
+        }
+    }
+    std::sort(veh_ptr.begin(), veh_ptr.end(), [](const auto& v1, const auto& v2){ return std::get<0>(v1)->arrival_time < std::get<0>(v2)->arrival_time; });
     ofile << "\n";
     ofile << y.queues[0].total_size << " " << y.queues[0].width << " " << y.queues.size();
     ofile << "\n\n";
-    for (int i = 0; i < y.queues.size(); i++) {
-        auto& q = y.queues[i];
-        for (int j = 0; j < q.vehicles.size(); j++) {
-            auto& v = q.vehicles[j];
-            ofile << v.type << " " << v.plate_nr << " " << v.arrival_time << " " << std::to_string(i) << " " << std::to_string(j) << " " << v.x << " " << v.y << " " << v.length << " " << v.width << " " << v.mass << "\n";
-        }
+    for (auto v_ptr : veh_ptr) {
+        auto& v = *std::get<0>(v_ptr);
+        ofile << v.type << " " << v.plate_nr << " " << v.arrival_time << " " << std::to_string(std::get<1>(v_ptr)) << " " << std::to_string(std::get<2>(v_ptr)) << " " << v.x << " " << v.y << " " << v.length << " " << v.width << " " << v.mass << "\n";
     }
 }
